e2e158cfe10b14329ae5bd4aa2be0cac87e6e64f
Samuel Schuhmacher <forgetaboutit@muhbaasu.de>
2015-08-19 21:19:35 +0200

    Remove obsolete `login` field from the users table

--- src/Pfennig/Schema.hs
+++ src/Pfennig/Schema.hs
@@ -11,7 +11,6 @@ createUsers =
      id serial primary key,
      created_at timestamp with time zone not null default now(),
      updated_at timestamp with time zone not null default now(),
-     login text not null,
      email text not null,
      password text not null,
      salt text not null)
