786161d7d1cd38735400bb52090ebc1e870d0c0f
Samuel Schuhmacher <forgetaboutit@muhbaasu.de>
2015-08-26 21:51:22 +0200

    Use MDL for styling

--- src/Pfennig/Layout.hs
+++ src/Pfennig/Layout.hs
@@ -22,50 +22,50 @@ pfennig :: Css
 pfennig = do
   html ? do
     boxSizing borderBox
-    backgroundColor backgroundColor'
-    color foregroundColor'
+    -- backgroundColor backgroundColor'
+    -- color foregroundColor'
 
   star ?
     boxSizing inherit
 
   base'
-  forms
+  -- forms
 
-  body ?
-    fstFontFamily
+  -- body ?
+  --   fstFontFamily
 
-  (h1 <> h2 <> h3) ?
-    color accentColor'
+  -- (h1 <> h2 <> h3) ?
+  --   color accentColor'
 
-blue' :: Color
-blue' = rgb 33 150 243
+-- blue' :: Color
+-- blue' = rgb 33 150 243
 
-purple' :: Color
-purple' = rgb 156 39 176
+-- purple' :: Color
+-- purple' = rgb 156 39 176
 
-backgroundColor' :: Color
-backgroundColor' = rgb 249 249 249
+-- backgroundColor' :: Color
+-- backgroundColor' = rgb 249 249 249
 
-foregroundColor' :: Color
-foregroundColor' = rgb 33 33 33
+-- foregroundColor' :: Color
+-- foregroundColor' = rgb 33 33 33
 
-lightGrey' :: Color
-lightGrey' = rgb 238 238 238
+-- lightGrey' :: Color
+-- lightGrey' = rgb 238 238 238
 
-accentColor' :: Color
-accentColor' = rgb 0 150 136
+-- accentColor' :: Color
+-- accentColor' = rgb 0 150 136
 
-shadowColor' :: Color
-shadowColor' = rgba 0 0 0 64
+-- shadowColor' :: Color
+-- shadowColor' = rgba 0 0 0 64
 
-button' :: Css
-button' =
-  button ? do
-    backgroundColor blue'
-    border none nil transparent
-    color white
-    padding (vh 0.75) (vw 2) (vh 0.75) (vw 2)
-    textTransform uppercase
+-- button' :: Css
+-- button' =
+--   button ? do
+--     backgroundColor blue'
+--     border none nil transparent
+--     color white
+--     padding (vh 0.75) (vw 2) (vh 0.75) (vw 2)
+--     textTransform uppercase
 
 base' :: Css
 base' = do
@@ -86,112 +86,112 @@ base' = do
     justifyContent center
     alignItems center
 
-  button'
-  input'
-  login'
+--   button'
+--   input'
+--   login'
 
-input' :: Css
-input' =
-  (input # ("type" ^= "password")) <> (input # ("type" ^= "text")) <? do
-    focus & do
-      outline none nil transparent
-      borderBottom double (px 1) accentColor'
+-- input' :: Css
+-- input' =
+--   (input # ("type" ^= "password")) <> (input # ("type" ^= "text")) <? do
+--     focus & do
+--       outline none nil transparent
+--       borderBottom double (px 1) accentColor'
 
-    border none nil transparent
-    borderBottom solid (px 1) lightGrey'
-    width $ pct 100
+--     border none nil transparent
+--     borderBottom solid (px 1) lightGrey'
+--     width $ pct 100
 
-forms :: Css
-forms = do
-  form ? do
-    background white
-    border solid (px 1) lightGrey'
-    paddingAll (vh 2)
+-- forms :: Css
+-- forms = do
+--   form ? do
+--     background white
+--     border solid (px 1) lightGrey'
+--     paddingAll (vh 2)
 
-  form |> ".row" ? do
-    justifyContent spaceBetween
-    margin nil nil (vh 1.5) nil
+--   form |> ".row" ? do
+--     justifyContent spaceBetween
+--     margin nil nil (vh 1.5) nil
 
-login' :: Css
-login' =
-  ".login" ? do
-    boxShadow' nil (vh 3) (vh 5) (vh (-1.5)) shadowColor'
-    width (pct 30)
+-- login' :: Css
+-- login' =
+--   ".login" ? do
+--     boxShadow' nil (vh 3) (vh 5) (vh (-1.5)) shadowColor'
+--     width (pct 30)
 
-boxShadow' :: Size a -> Size a -> Size a -> Size a -> Color -> Css
-boxShadow' x y w z c = prefixed (browsers <> "box-shadow") (x ! y ! w ! z ! c)
+-- boxShadow' :: Size a -> Size a -> Size a -> Size a -> Color -> Css
+-- boxShadow' x y w z c = prefixed (browsers <> "box-shadow") (x ! y ! w ! z ! c)
 
-fstFontFamily :: Css
-fstFontFamily = fontFamily ["Roboto"] [sansSerif]
+-- fstFontFamily :: Css
+-- fstFontFamily = fontFamily ["Roboto"] [sansSerif]
 
-paddingAll :: Size a -> Css
-paddingAll s' = padding s' s' s' s'
+-- paddingAll :: Size a -> Css
+-- paddingAll s' = padding s' s' s' s'
 
---------------------------------------------------------------------------------
---- http://csswizardry.com/2012/02/pragmatic-practical-font-sizing-in-css/
+-- --------------------------------------------------------------------------------
+-- --- http://csswizardry.com/2012/02/pragmatic-practical-font-sizing-in-css/
 
-fonts :: Css
-fonts = do
-  giga
-  mega
-  kilo
-  alpha
-  beta
-  gamma
-  delta
-  epsilon
-  milli
+-- fonts :: Css
+-- fonts = do
+--   giga
+--   mega
+--   kilo
+--   alpha
+--   beta
+--   gamma
+--   delta
+--   epsilon
+--   milli
 
-giga :: Css
-giga = ".giga" ? do
-  fontSize (px 80)
-  fontSize (rem 5)
-  lineHeight (em 1.2)
+-- giga :: Css
+-- giga = ".giga" ? do
+--   fontSize (px 80)
+--   fontSize (rem 5)
+--   lineHeight (em 1.2)
 
-mega :: Css
-mega = ".mega" ? do
-  fontSize (px 64)
-  fontSize (rem 4)
-  lineHeight (em 1.125)
+-- mega :: Css
+-- mega = ".mega" ? do
+--   fontSize (px 64)
+--   fontSize (rem 4)
+--   lineHeight (em 1.125)
 
-kilo :: Css
-kilo = ".kilo" ? do
-  fontSize (px 48)
-  fontSize (rem 3)
-  lineHeight (em 1)
+-- kilo :: Css
+-- kilo = ".kilo" ? do
+--   fontSize (px 48)
+--   fontSize (rem 3)
+--   lineHeight (em 1)
 
-alpha :: Css
-alpha = h1 <> ".alpha" ? do
-  fontSize (px 32)
-  fontSize (rem 2)
-  lineHeight (em 1.5)
+-- alpha :: Css
+-- alpha = h1 <> ".alpha" ? do
+--   fontSize (px 32)
+--   fontSize (rem 2)
+--   lineHeight (em 1.5)
 
-beta :: Css
-beta = h2 <> ".beta" ? do
-  fontSize (px 24)
-  fontSize (rem 1.5)
-  lineHeight (em 1)
+-- beta :: Css
+-- beta = h2 <> ".beta" ? do
+--   fontSize (px 24)
+--   fontSize (rem 1.5)
+--   lineHeight (em 1)
 
-gamma :: Css
-gamma = h3 <> ".gamma" ? do
-  fontSize (px 20)
-  fontSize (rem 1.25)
-  lineHeight (em 1.2)
+-- gamma :: Css
+-- gamma = h3 <> ".gamma" ? do
+--   fontSize (px 20)
+--   fontSize (rem 1.25)
+--   lineHeight (em 1.2)
 
-delta :: Css
-delta = h4 <> ".delta" ? do
-  fontSize (px 18)
-  fontSize (rem 1.125)
-  lineHeight (em 1.333)
+-- delta :: Css
+-- delta = h4 <> ".delta" ? do
+--   fontSize (px 18)
+--   fontSize (rem 1.125)
+--   lineHeight (em 1.333)
 
-epsilon :: Css
-epsilon = h5 <> ".epsilon" <> h6 <> ".zeta" ? do
-  fontSize (px 16)
-  fontSize (rem 1)
-  lineHeight (em 1.5)
+-- epsilon :: Css
+-- epsilon = h5 <> ".epsilon" <> h6 <> ".zeta" ? do
+--   fontSize (px 16)
+--   fontSize (rem 1)
+--   lineHeight (em 1.5)
 
-milli :: Css
-milli = ".milli" ? do
-  fontSize (px 12)
-  fontSize (rem 0.75)
-  lineHeight (em 2)
+-- milli :: Css
+-- milli = ".milli" ? do
+--   fontSize (px 12)
+--   fontSize (rem 0.75)
+--   lineHeight (em 2)
--- src/Pfennig/View.hs
+++ src/Pfennig/View.hs
@@ -23,6 +23,12 @@ hd = do
          href_ "assets/normalize.css"]
   link_ [rel_ "stylesheet",
          type_ "text/css",
+         href_ "https://storage.googleapis.com/code.getmdl.io/1.0.3/material.indigo-pink.min.css"]
+  script_ [src_ "https://storage.googleapis.com/code.getmdl.io/1.0.3/material.min.js"] ""
+  link_ [rel_ "stylesheet",
+         href_ "https://fonts.googleapis.com/icon?family=Material+Icons"]
+  link_ [rel_ "stylesheet",
+         type_ "text/css",
          href_ "assets/generated.css"]
 
 main' :: Html ()
@@ -36,40 +42,69 @@ login =
     form_ [class_ "login", method_ "post", action_ "/login"] $ do
       h3_ "Login"
 
-      div_ [class_ "row"] $
-        input_ [id_ "email", name_ "email",
-                placeholder_ "E-Mail",
+      div_ [class_ "row mdl-textfield mdl-js-textfield mdl-textfield--floating-label"] $ do
+        input_ [id_ "email",
+                class_ "mdl-textfield__input",
+                name_ "email",
                 type_ "text"]
+        label_ [class_ "mdl-textfield__label",
+                for_ "email"] "E-Mail"
 
-      div_ [class_ "row"] $
-        input_ [id_ "pass", name_ "pass",
-                placeholder_ "Password",
+      div_ [class_ "row mdl-textfield mdl-js-textfield mdl-textfield--floating-label"] $ do
+        input_ [id_ "pass",
+                class_ "mdl-textfield__input",
+                name_ "pass",
                 type_ "password"]
+        label_ [class_ "mdl-textfield__label",
+                for_ "pass"] "Password"
+
+      div_ [class_ "row mdl-textfield"] $ do
+        label_ [for_ "remember",
+                class_ "mdl-checkbox mdl-js-checkbox mdl-js-ripple-effect"] $ do
+          input_ [id_ "remember",
+                  type_ "checkbox",
+                  name_ "remember",
+                  value_ "remember",
+                  class_ "mdl-checkbox__input"]
+          span_ [class_ "mdl-checkbox__label"] "Remember me"
 
       div_ [class_ "row"] $ do
-        label_ [for_ "remember"] "Remember me"
-        input_ [id_ "remember",
-                type_ "checkbox",
-                name_ "remember",
-                value_ "remember"]
-
-      div_ [class_ "row"] $ do
-        a_ [href_ "/register"] "Register"
-        a_ [href_ "#"] "Forgot password?"
+        a_ [href_ "/register",
+            class_ "mdl-button mdl-js-button mdl-button--raised mdl-js-ripple-effect"] "Register"
+        a_ [href_ "#",
+            class_ "mdl-button mdl-js-button mdl-button--raised mdl-js-ripple-effect"] "Forgot password?"
 
       div_ [class_ "row"] $
         button_ [type_ "submit",
+                 class_ "mdl-button mdl-js-button mdl-button--raised mdl-js-ripple-effect mdl-button--colored",
                  value_ "submit"] "Login"
 
 register :: Html ()
 register =
   div_ [class_ "center"] $
     form_ [class_ "registration", method_ "post", action_ "/register"] $ do
-      div_ [class_ "row"] $ do
-        label_ [for_ "email"] "E-Mail"
-        input_ [id_ "email", name_ "email", type_ "text"]
-      div_ [class_ "row"] $ do
-        label_ [for_ "pass"] "Password"
-        input_ [id_ "pass", name_ "pass", type_ "password"]
+      h3_ "Register"
+
+      div_ [class_ "row mdl-textfield mdl-js-textfield mdl-textfield--floating-label"] $ do
+        input_ [id_ "email",
+                class_ "mdl-textfield__input",
+                name_ "email",
+                type_ "email",
+                required_ ""]
+        label_ [class_ "mdl-textfield__label",
+                for_ "email"] "E-Mail"
+
+      div_ [class_ "row mdl-textfield mdl-js-textfield mdl-textfield--floating-label"] $ do
+        input_ [id_ "pass",
+                class_ "mdl-textfield__input",
+                name_ "pass",
+                pattern_ "(\\w|\\s){5,}",
+                required_ "",
+                type_ "password"]
+        label_ [class_ "mdl-textfield__label",
+                for_ "pass"] "Password"
+
       div_ [class_ "row"] $
-        button_ [type_ "submit"] "Register"
+        button_ [type_ "submit",
+                 class_ "mdl-button mdl-js-button mdl-button--raised mdl-js-ripple-effect mdl-button--colored",
+                 value_ "submit"] "Register"
